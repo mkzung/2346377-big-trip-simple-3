5e6c4ca354fb59b91f1d9e474af930ac465f5094
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 17:52:34 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Filters from './view/filters.js';
-import {render} from './render.js';
 import BoardPresenter from './presenter/board-presenter';
 import ModelWaypoint from './model/model-waypoint';
-import {mockInit} from './mock/point';
+import {mockInit, waypoints} from './mock/point';
+import {render} from './framework/render';
 
 const siteHeaderElement = document.querySelector('.trip-controls__filters');
 const container = document.querySelector('.trip-events');
 
-mockInit(6, 10);
-const modelWaypoints = new ModelWaypoint();
+mockInit(5, 10);
+const modelWaypoints = new ModelWaypoint(waypoints);
 const boardPresenter = new BoardPresenter({boardContainer: container, waypointsModel: modelWaypoints});
 
 render(new Filters(), siteHeaderElement);
