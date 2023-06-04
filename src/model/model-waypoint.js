ee8b6f67ab89658ad05f3bd526ec74465f22ec06
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-05 00:25:51 +0300

    Update model-waypoint.js

--- src/model/model-waypoint.js
+++ src/model/model-waypoint.js
@@ -18,7 +18,7 @@ export default class ModelWaypoint extends Observable {
     try {
       const waypoints = await this.#waypointsApiService.waypoints;
       this.#waypoints = waypoints.map(this.#adaptToClient);
-    } catch(err) {
+    } catch (err) {
       this.#waypoints = [];
     }
     this._notify(UpdateType.INIT);
@@ -40,37 +40,44 @@ export default class ModelWaypoint extends Observable {
         ...this.#waypoints.slice(index + 1),
       ];
       this._notify(updateType, updatedWaypoint);
-    } catch(err) {
+    } catch (err) {
       throw new Error('Can\'t update waypoint');
     }
   }
 
-  addWaypoint(updateType, update) {
-    this.#waypoints = [
-      update,
-      ...this.#waypoints
-    ];
-
-    this._notify(updateType, update);
+  async addWaypoint(updateType, update) {
+    try {
+      const response = await this.#waypointsApiService.addWaypoint(update);
+      const newWaypoint = this.#adaptToClient(response);
+      this.#waypoints = [newWaypoint, ...this.#waypoints];
+      this._notify(updateType, newWaypoint);
+    } catch (err) {
+      throw new Error('Can\'t add waypoint');
+    }
   }
 
-  deleteWaypoint = (updateType, update) => {
+  async deleteWaypoint(updateType, update) {
     const index = this.#waypoints.findIndex((waypont) => waypont.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t delete unexisting waypoint');
     }
 
-    this.#waypoints = [
-      ...this.#waypoints.slice(0, index),
-      ...this.#waypoints.slice(index + 1),
-    ];
-
-    this._notify(updateType);
-  };
+    try {
+      await this.#waypointsApiService.deleteWaypoint(update);
+      this.#waypoints = [
+        ...this.#waypoints.slice(0, index),
+        ...this.#waypoints.slice(index + 1),
+      ];
+      this._notify(updateType);
+    } catch (err) {
+      throw new Error('Can\'t delete waypoint');
+    }
+  }
 
   #adaptToClient(waypoint) {
-    const adaptedWaypoint = {...waypoint,
+    const adaptedWaypoint = {
+      ...waypoint,
       dateFrom: waypoint['date_from'],
       dateTo: waypoint['date_to'],
       offersIDs: waypoint['offers'],
