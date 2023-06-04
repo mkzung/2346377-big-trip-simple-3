d7f666a6600ab0eac4a00fe141a0f783a7bdcb1c
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 18:59:23 +0300

    Update sorting.js

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,11 +1,32 @@
 import AbstractView from '../framework/view/abstract-view';
+import { changeType } from '../utils';
 
-function createNoWaypoitsMessageTemplate() {
-  return '<p class="trip-events__msg">Click New Event to create your first point</p>';
+function createSortItemTemplate(sortType) {
+  return `
+  <div class="trip-sort__item  trip-sort__item--${sortType}">
+    <input id="sort-${sortType}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sortType}">
+    <label class="trip-sort__btn" for="sort-${sortType}">${changeType(sortType)}</label>
+  </div>`;
 }
 
-export default class NoWaypointMessage extends AbstractView {
+function createSortingTemplate(sorts) {
+  const sortItemsTemplate = sorts.map((sortType) => createSortItemTemplate(sortType)).join('');
+  return (`
+  <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    ${sortItemsTemplate}
+  </form>`
+  );
+}
+
+export default class Sorting extends AbstractView {
+  #sorts = null;
+
+  constructor(sorts) {
+    super();
+    this.#sorts = sorts;
+  }
+
   get template() {
-    return createNoWaypoitsMessageTemplate();
+    return createSortingTemplate(this.#sorts);
   }
 }
