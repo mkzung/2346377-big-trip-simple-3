9ffe5e569db7b952bb821c5f6e2920ed78e1c94a
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-05 00:22:07 +0300

    Update no-waypoints.js

--- src/view/no-waypoints.js
+++ src/view/no-waypoints.js
@@ -1,5 +1,5 @@
 import AbstractView from '../framework/view/abstract-view';
-import {FilterType} from '../mock/const';
+import {FilterType} from '../const';
 
 const NoTasksTextType = {
   [FilterType.EVERYTHING]: 'Click New Event to create your first point',
