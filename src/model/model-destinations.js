9f52dbcd47d872b3fca330adae1e29d0776a44ef
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 23:54:12 +0300

    Update model-destinations.js

--- src/model/model-destinations.js
+++ src/model/model-destinations.js
@@ -1,11 +1,21 @@
 import Observable from '../framework/observable';
 
 export default class ModelDestinations extends Observable {
+  #waypointsApiService = null;
   #destinations = [];
 
-  constructor(destinations) {
+  constructor({waypointsApiService}) {
     super();
-    this.#destinations = destinations;
+    this.#waypointsApiService = waypointsApiService;
+    this.init();
+  }
+
+  async init() {
+    try {
+      this.#destinations = await this.#waypointsApiService.destinations;
+    } catch(err) {
+      this.#destinations = [];
+    }
   }
 
   get destinations() {
