fb2f7dcbc7e03d869a6c0e479c4336404898e513
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 19:05:49 +0300

    Update filters.js

--- src/view/filters.js
+++ src/view/filters.js
@@ -1,34 +1,96 @@
-import AbstractView from '../framework/view/abstract-view';
-import { changeType } from '../utils.js';
+import {render, replace, remove} from '../framework/render';
+import WaypointView from '../view/waypoint';
+import EditForm from '../view/edit-form';
+import {isEsc} from '../utils';
 
-function createFilterItemTemplate(filterType) {
-  return `
-  <div class="trip-filters__filter">
-      <input id="filter-${filterType}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filterType}">
-      <label class="trip-filters__filter-label" for="filter-${filterType}">${changeType(filterType)}</label>
-  </div>
-  `;
-}
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING'
+};
 
-function createFilterTemplate(filters) {
-  const filterItems = filters.map((filter) => createFilterItemTemplate(filter)).join('');
-  return (`
-    <form class="trip-filters" action="#" method="get">
-      ${filterItems}
-      <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>`
-  );
-}
+export default class WaypointPresenter {
+  #handleModeChange = null;
+  #waypointList = null;
+  #editFormComponent = null;
+  #waypointComponent = null;
+  #waypoint = null;
+  #mode = Mode.DEFAULT;
 
-export default class Filters extends AbstractView {
-  #filters = null;
-
-  constructor(filters) {
-    super();
-    this.#filters = filters;
+  constructor({waypointList, onModeChange}) {
+    this.#waypointList = waypointList;
+    this.#handleModeChange = onModeChange;
   }
 
-  get template() {
-    return createFilterTemplate(this.#filters);
+  init(waypoint) {
+    this.#waypoint = waypoint;
+
+    const prevWaypointComponent = this.#waypointComponent;
+    const prevEditFormComponent = this.#editFormComponent;
+
+    this.#waypointComponent = new WaypointView({
+      oneWaypoint: this.#waypoint,
+      onClick: this.#handleEditClick
+    });
+
+    this.#editFormComponent = new EditForm({
+      oneWaypoint: waypoint,
+      onSubmit: this.#handleFormSubmit
+    });
+
+    if (prevWaypointComponent === null || prevEditFormComponent === null) {
+      render(this.#waypointComponent, this.#waypointList);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) {
+      replace(this.#waypointComponent, prevWaypointComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#editFormComponent, prevEditFormComponent);
+    }
+
+    remove(prevEditFormComponent);
+    remove(prevWaypointComponent);
   }
+
+  destroy() {
+    remove(this.#waypointComponent);
+    remove(this.#editFormComponent);
+  }
+
+  resetView() {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceFormToPoint();
+    }
+  }
+
+  #replacePointToForm = () => {
+    replace(this.#editFormComponent, this.#waypointComponent);
+    this.#handleModeChange();
+    this.#mode = Mode.EDITING;
+  };
+
+  #replaceFormToPoint = () => {
+    replace(this.#waypointComponent, this.#editFormComponent);
+    this.#mode = Mode.DEFAULT;
+  };
+
+  #ecsKeydown = (evt) => {
+    if (isEsc(evt)) {
+      evt.preventDefault();
+      this.#replaceFormToPoint();
+      document.body.removeEventListener('keydown', this.#ecsKeydown);
+    }
+  };
+
+  #handleEditClick = () => {
+    this.#replacePointToForm();
+    document.body.addEventListener('keydown', this.#ecsKeydown);
+  };
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToPoint();
+    document.body.removeEventListener('keydown', this.#ecsKeydown);
+  };
 }
