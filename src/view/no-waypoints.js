42e450a3de26fd968b92fc0f6e2a8daef4ec1f20
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 22:32:09 +0300

    Update no-waypoints.js

--- src/view/no-waypoints.js
+++ src/view/no-waypoints.js
@@ -1,11 +1,25 @@
 import AbstractView from '../framework/view/abstract-view';
+import {FilterType} from '../mock/const';
 
-function createNoWaypoitsMessageTemplate() {
-  return '<p class="trip-events__msg">Click New Event to create your first point</p>';
+const NoTasksTextType = {
+  [FilterType.EVERYTHING]: 'Click New Event to create your first point',
+  [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PAST]: 'There are no past events now',
+};
+
+function createNoWaypoitsMessageTemplate(filterType) {
+  return `<p class="trip-events__msg">${NoTasksTextType[filterType]}</p>`;
 }
 
 export default class NoWaypointMessage extends AbstractView {
+  #filterType = null;
+
+  constructor({filterType}) {
+    super();
+    this.#filterType = filterType;
+  }
+
   get template() {
-    return createNoWaypoitsMessageTemplate();
+    return createNoWaypoitsMessageTemplate(this.#filterType);
   }
 }
