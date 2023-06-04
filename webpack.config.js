1218b2c0798c3ff1bfd1b1e3fffb6a2cb822215c
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 14:10:50 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: 'public'},
+      ],
+    }),
+  ],
+
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        {loader: 'babel-loader'}
+      ]
+    }]
+  }
+};
