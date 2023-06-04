752a5ff89fd52252dec2ce184d6060a306b5ed54
mkzung <103102868+mkzung@users.noreply.github.com>
2023-06-04 23:52:28 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
 import BoardPresenter from './presenter/board-presenter';
 import ModelWaypoint from './model/model-waypoint';
-import {mockInit, waypoints} from './mock/point';
 import ModelOffers from './model/model-offers';
 import ModelDestinations from './model/model-destinations';
-import {offersByType} from './mock/const';
-import {destinations} from './mock/destination';
 import ModelFilters from './model/model-filter';
 import FilterPresenter from './presenter/filter-presenter';
 import {render} from './render';
 import NewWaypointButton from './view/new-waypoint-button';
+import WaypointsApiService from './waypoints-api-service';
 
 const siteHeaderElement = document.querySelector('.trip-controls__filters');
 const container = document.querySelector('.trip-events');
 const placeForButton = document.querySelector('.trip-main');
 
-mockInit(3, 10);
-const modelWaypoints = new ModelWaypoint(waypoints);
-const modelOffers = new ModelOffers(offersByType);
-const modelDestinations = new ModelDestinations(destinations);
+const AUTHORIZATION = 'Basic sgkdajgskdgas7777';
+const END_POINT = 'https://18.ecmascript.pages.academy/big-trip';
+
+const waypointsApiService = new WaypointsApiService(END_POINT, AUTHORIZATION);
+
+const modelWaypoints = new ModelWaypoint({waypointsApiService});
+const modelOffers = new ModelOffers({waypointsApiService});
+const modelDestinations = new ModelDestinations({waypointsApiService});
 const modelFilter = new ModelFilters();
 
 const boardPresenter = new BoardPresenter({
@@ -48,7 +50,9 @@ function handleNewTaskButtonClick() {
   newWaypointButtonComponent.element.disabled = true;
 }
 
-render(newWaypointButtonComponent, placeForButton);
-
 filterPresenter.init();
 boardPresenter.init();
+modelWaypoints.init()
+  .finally(() => {
+    render(newWaypointButtonComponent, placeForButton);
+  });
